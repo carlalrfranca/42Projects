26c3c1d075a23d4b79ade6046a207cbbb2ad0f67
carlarfranca <71091956+carlarfranca@users.noreply.github.com>
2022-06-08 22:51:35 -0300

    Update

--- philosophers/inc/philo.h
+++ philosophers/inc/philo.h
@@ -26,42 +26,42 @@ typedef struct  s_principal t_principal;
 
 typedef struct s_ph
 {
-	int				philo;
-	int				meal;
-	long long 		last_meal;
-	int				left_fork;
-	int				right_fork;
-	pthread_t		thread;
-	t_principal		*ret_principal;
+	int		philo;
+	int		meal;
+	long long 	last_meal;
+	int		left_fork;
+	int		right_fork;
+	pthread_t	thread;
+	t_principal	*ret_principal;
 }	t_ph;
 
 struct s_principal
 {
-	int				nb_ph;
-	int				tm_to_die;
-	int				tm_to_eat;
-	int				tm_to_sleep;
-	int				nb_eat;
-	long long		start_meal;
-	int				checker;
-	int				full_philos;
-	t_ph			*ph;
-	pthread_t		monitor;
+	int		nb_ph;
+	int		tm_to_die;
+	int		tm_to_eat;
+	int		tm_to_sleep;
+	int		nb_eat;
+	long long	start_meal;
+	int		checker;
+	int		full_philos;
+	t_ph		*ph;
+	pthread_t	monitor;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	print;
 };
 
-int			ft_exit(t_principal *input, char *str);
+int		ft_exit(t_principal *input, char *str);
 void		ft_putstr_fd(char *s, int fd);
-int			ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 void		*has_died(void *param);
-int			management_threads(t_principal *input);
+int		management_threads(t_principal *input);
 void		eat(t_ph *philo);
 void		*one_philo(t_ph *philo);
 long long	get_time(void);
 void		*start_routine(void *param);
-int			create_philos(t_principal *input);
-int			parse_args(int argc, char **argv, t_principal *input);
+int		create_philos(t_principal *input);
+int		parse_args(int argc, char **argv, t_principal *input);
 void		init_args_struct(t_principal *input);
 void		check_args(int argc, t_principal *input);
 
